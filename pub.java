372f29564189358e34d71893c9f5032d431f36b6
Swaraj <kaleswaraj321@gmail.com>
2024-01-20 16:06:34 +0530

    first commit

---
+++ pub.java
@@ -0,0 +1,20 @@
+Class Demo
+        {
+        void show()
+        System.out.println("Welcome");
+        }
+        {
+        Void show(int x);
+        System.out.printlN(x);
+        }
+        {
+        Void show(float);
+        System.out.println(Y);
+        }
+        {
+        Public ststic void main(String abc);
+        Demo D=new Demo();
+        D.show(2.9);
+        D.show(7);
+        D.show();
+        }
